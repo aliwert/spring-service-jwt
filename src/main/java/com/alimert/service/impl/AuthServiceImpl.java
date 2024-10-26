b4c6d7e27662dc6182cc079d7861ada034eda15f
aliwert <154356044+aliwert@users.noreply.github.com>
2024-10-26 21:13:34 +0300

    feat: encode password

--- src/main/java/com/alimert/service/impl/AuthServiceImpl.java
+++ src/main/java/com/alimert/service/impl/AuthServiceImpl.java
@@ -1,5 +1,6 @@
 package com.alimert.service.impl;
 
+import com.alimert.config.AppConfig;
 import com.alimert.dto.DtoUser;
 import com.alimert.jwt.AuthRequest;
 import com.alimert.model.User;
@@ -7,6 +8,7 @@ import com.alimert.repository.UserRepository;
 import com.alimert.service.IAuthService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,13 +17,16 @@ public class AuthServiceImpl implements IAuthService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
 
     @Override
     public DtoUser register(AuthRequest authRequest) {
         DtoUser dtoUser = new DtoUser();
         User user = new User();
         user.setUsername(authRequest.getUsername());
-        user.setPassword(authRequest.getPassword());
+        user.setPassword(encoder.encode(authRequest.getPassword()));
         User savedUser = userRepository.save(user);
         BeanUtils.copyProperties(savedUser, dtoUser);
 
