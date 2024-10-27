ece0a2e7f132b35375948c87e0958637c3860098
aliwert <154356044+aliwert@users.noreply.github.com>
2024-10-27 22:46:00 +0300

    feat: add swagger

--- src/main/java/com/alimert/config/SecurityConfig.java
+++ src/main/java/com/alimert/config/SecurityConfig.java
@@ -20,6 +20,11 @@ public class SecurityConfig {
     public static final String AUTHENTICATE = "/authenticate";
     public static final String REGISTER = "/register";
     public static final String REFRESH_TOKEN = "/refreshtoken";
+    public static final String[] SWAGGER_PATHS = {
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-ui.html"
+    };
 
     @Autowired
     private AuthenticationProvider authenticationProvider;
@@ -35,6 +40,7 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(AUTHENTICATE, REGISTER, REFRESH_TOKEN).permitAll()
+                        .requestMatchers(SWAGGER_PATHS).permitAll()
                         .anyRequest().authenticated())
                 .exceptionHandling(exception -> exception
                         .authenticationEntryPoint(authEntryPoint))
